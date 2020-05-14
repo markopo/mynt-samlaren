d1a2956b4b49f5f17db2754f1043f9e7717b1501
Marko <marko_poikkimaki@hotmail.com>
2020-05-14 17:48:02 +0200

    moving the fox

--- mynt.py
+++ mynt.py
@@ -6,11 +6,25 @@ HEIGHT = 400
 summa = 0
 spelet_slut = False
 
+
+max_x = WIDTH - 20
+min_x = 20
+
+min_y = 20
+max_y = HEIGHT - 20
+
 rav = Actor("fox")
 rav.pos = 100, 100
 
 mynt = Actor("coin")
-mynt.pos = 200, 200
+mynt.pos = 20, 300
+
+
+
+def tiden_slut():
+    print("tiden slut")
+    spelet_slut = True
+
 
 def draw():
     screen.clear()
@@ -19,17 +33,31 @@ def draw():
     mynt.draw()
     screen.draw.text("Summa: " + str(summa), color="black", topleft=(10,10))
 
+def on_key_down(key):
+    print(key)    
 
-def placera_mynt():
-    print("placera mynt")
-
-
-def tiden_slut():
-    print("tiden slut")
 
 
 def update():
-    placera_mynt()
+
+    if mynt.x < max_x:
+            mynt.x = mynt.x + 1
+
+    
+    if keyboard.W:
+        if rav.y > min_y:
+            rav.y = rav.y - 2
+    elif keyboard.S:
+        if rav.y < max_y:
+            rav.y = rav.y + 2 
+    elif keyboard.A:
+        if rav.x > min_x:
+            rav.x = rav.x - 2
+    elif keyboard.D:
+        if rav.x < max_x:
+            rav.x = rav.x + 2 
+
+   
     
 
 
